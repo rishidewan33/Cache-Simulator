cc1e8ea2cbc68704a1a4775db6e64b3a79b32ebe
Rishi Dewan <rishidewan33@gmail.com>
2011-05-26 15:43:53 -0500

    Cache Simulator Source Files

---
+++ MissComb.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cache;
+
+/**
+ *
+ * @author Rishi
+ */
+public class MissComb
+{
+    int ass;
+    int cap;
+    int block;
+    double miss;
+    int readm;
+    int writem;
+
+    public MissComb(int a, int c, int b, double m)
+    {
+        ass = a;
+        cap = c;
+        block = b;
+        miss = m;
+    }
+    public double getMiss()
+    {
+        return miss;
+    }
+    public String toMissString()
+    {
+        return "MissRate: "+miss+" Capacity: "+cap+" Associativity: "+ass+" Block Size: "+block;
+    }
+}
